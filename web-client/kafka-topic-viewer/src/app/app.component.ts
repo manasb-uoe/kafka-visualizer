2e69c2940e8902816c0c2d2ebeb1bc79315f49dc
enthusiast94 <manas.bajaj94@gmail.com>
2017-08-28 16:03:18 +0100

    Deleted unused files.

--- web-client/kafka-topic-viewer/src/app/app.component.ts
+++ web-client/kafka-topic-viewer/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Hello world</h1> 
+  `
 })
 export class AppComponent {
   title = 'app';
