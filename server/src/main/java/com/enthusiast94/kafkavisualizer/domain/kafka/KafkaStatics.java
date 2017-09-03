06a930680088a55a767fcbb07afd7c4c35b91dab
enthusiast94 <manas.bajaj94@gmail.com>
2017-09-03 21:12:46 +0100

    Deleted old server code.

--- server/src/main/java/com/enthusiast94/kafkavisualizer/domain/kafka/KafkaStatics.java
+++
@@ -1,5 +0,0 @@
-package com.enthusiast94.kafkavisualizer.domain.kafka;
-
-public class KafkaStatics {
-    public static String GROUP_ID = "VisualizerGroupId";
-}
--- server/src/main/java/com/enthusiast94/kafkavisualizer/domain/zookeeper/ZookeeperNode.java
+++
@@ -1,6 +0,0 @@
-package com.enthusiast94.kafkavisualizer.domain.zookeeper;
-
-public class ZookeeperNode {
-    public String hostname;
-    public int port;
-}
